712e0842f195d03cf9daffcb8d45585d4e1d8973
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2025-07-14 12:52:36 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { WalletService } from "./services/walletService";
+
+function App() {
+  const [accountId, setAccountId] = useState<string | null>(null);
+  const connectWallet = async () => {
+    await WalletService.init();
+
+    const checkConnected = setInterval(() => {
+      console.log("Checking connection...", WalletService.isConnected());
+      if (WalletService.isConnected()) {
+        setAccountId(WalletService.getAccountId());
+        clearInterval(checkConnected);
+      }
+    }, 500);
+  };
+
+  const disconnect = async () => {
+    await WalletService.disconnect()
+    if(!WalletService.isConnected()) {
+      setAccountId(null)
+    }
+  }
+
+  return (
+    <div>
+      <h1>test</h1>
+      {accountId ? (
+        <p>
+          Wallet Connected: <strong>{accountId}</strong>
+          <button onClick={disconnect}>Disconnect</button>
+        </p>
+      ) : (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/walletService.ts
@@ -0,0 +1,70 @@
+import { LedgerId } from "@hashgraph/sdk";
+import {
+  HashConnect,
+  HashConnectConnectionState,
+  type SessionData,
+} from "hashconnect";
+
+const appMetadata = {
+  name: "ChainLink",
+  description: "ChainLink description here",
+  icons: [
+    "https://avatars.githubusercontent.com/u/114144117?s=400&u=b43effb8c13cfb3971f5dd9f1b10bebbf1e9711c&v=4",
+  ],
+  url: "http://localhost:5173/",
+};
+
+const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID;
+
+let hashconnect: HashConnect;
+let state: HashConnectConnectionState = HashConnectConnectionState.Disconnected;
+let pairingData: SessionData | null = null;
+
+export const WalletService = {
+  async init() {
+    hashconnect = new HashConnect(
+      LedgerId.TESTNET,
+      projectId,
+      appMetadata,
+      true
+    );
+
+    this.setUpHashConnectEvents();
+
+    await hashconnect.init();
+
+    if (!hashconnect.connectedAccountIds[0]) {
+      hashconnect.openPairingModal();
+    }
+  },
+
+  setUpHashConnectEvents() {
+    hashconnect.pairingEvent.on((newPairing) => {
+      console.log("Paired:", newPairing);
+      pairingData = newPairing;
+    });
+
+    hashconnect.disconnectionEvent.on(() => {
+      console.log("Disconnected");
+      state = HashConnectConnectionState.Disconnected
+      pairingData = null;
+    });
+
+    hashconnect.connectionStatusChangeEvent.on((connectionStatus) => {
+      console.log("Connection state:", connectionStatus);
+      state = connectionStatus;
+    });
+  },
+
+  async disconnect() {
+    await hashconnect.disconnect()
+  },
+
+  getAccountId(): string | null {
+    return pairingData?.accountIds[0] ?? null;
+  },
+
+  isConnected(): boolean {
+    return state === HashConnectConnectionState.Paired && pairingData !== null;
+  },
+};
